f4a1f0c9d5bcd2dc27b721ff7d69e492865a0ba4
Samantha Zhang <sammie@dhcp-clara-1083.eduroam.cornell.edu>
2020-02-05 15:22:02 -0500

    First commit

---
+++ Game of 21/Game of 21/Form1.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Lblp5 = New System.Windows.Forms.Label()
+        Me.Lblp1 = New System.Windows.Forms.Label()
+        Me.Lblc2 = New System.Windows.Forms.Label()
+        Me.Lblpscore = New System.Windows.Forms.Label()
+        Me.Lblp2 = New System.Windows.Forms.Label()
+        Me.Lblcscore = New System.Windows.Forms.Label()
+        Me.Lblp4 = New System.Windows.Forms.Label()
+        Me.Lblc3 = New System.Windows.Forms.Label()
+        Me.Lblc1 = New System.Windows.Forms.Label()
+        Me.LblCp = New System.Windows.Forms.Label()
+        Me.LblCDealt = New System.Windows.Forms.Label()
+        Me.LblPlayer = New System.Windows.Forms.Label()
+        Me.LblPDealt = New System.Windows.Forms.Label()
+        Me.Lblp3 = New System.Windows.Forms.Label()
+        Me.btnDraw = New System.Windows.Forms.Button()
+        Me.btnCheck = New System.Windows.Forms.Button()
+        Me.btnPlayAgn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Lblp5
+        '
+        Me.Lblp5.AutoSize = True
+        Me.Lblp5.Location = New System.Drawing.Point(238, 29)
+        Me.Lblp5.Name = "Lblp5"
+        Me.Lblp5.Size = New System.Drawing.Size(25, 13)
+        Me.Lblp5.TabIndex = 13
+        Me.Lblp5.Text = "___"
+        '
+        'Lblp1
+        '
+        Me.Lblp1.AutoSize = True
+        Me.Lblp1.Location = New System.Drawing.Point(118, 29)
+        Me.Lblp1.Name = "Lblp1"
+        Me.Lblp1.Size = New System.Drawing.Size(25, 13)
+        Me.Lblp1.TabIndex = 12
+        Me.Lblp1.Text = "___"
+        '
+        'Lblc2
+        '
+        Me.Lblc2.AutoSize = True
+        Me.Lblc2.Location = New System.Drawing.Point(168, 186)
+        Me.Lblc2.Name = "Lblc2"
+        Me.Lblc2.Size = New System.Drawing.Size(25, 13)
+        Me.Lblc2.TabIndex = 11
+        Me.Lblc2.Text = "___"
+        '
+        'Lblpscore
+        '
+        Me.Lblpscore.AutoSize = True
+        Me.Lblpscore.Location = New System.Drawing.Point(100, 69)
+        Me.Lblpscore.Name = "Lblpscore"
+        Me.Lblpscore.Size = New System.Drawing.Size(25, 13)
+        Me.Lblpscore.TabIndex = 10
+        Me.Lblpscore.Text = "___"
+        '
+        'Lblp2
+        '
+        Me.Lblp2.AutoSize = True
+        Me.Lblp2.Location = New System.Drawing.Point(149, 29)
+        Me.Lblp2.Name = "Lblp2"
+        Me.Lblp2.Size = New System.Drawing.Size(25, 13)
+        Me.Lblp2.TabIndex = 8
+        Me.Lblp2.Text = "___"
+        '
+        'Lblcscore
+        '
+        Me.Lblcscore.AutoSize = True
+        Me.Lblcscore.Location = New System.Drawing.Point(117, 212)
+        Me.Lblcscore.Name = "Lblcscore"
+        Me.Lblcscore.Size = New System.Drawing.Size(25, 13)
+        Me.Lblcscore.TabIndex = 7
+        Me.Lblcscore.Text = "___"
+        '
+        'Lblp4
+        '
+        Me.Lblp4.AutoSize = True
+        Me.Lblp4.Location = New System.Drawing.Point(207, 29)
+        Me.Lblp4.Name = "Lblp4"
+        Me.Lblp4.Size = New System.Drawing.Size(25, 13)
+        Me.Lblp4.TabIndex = 6
+        Me.Lblp4.Text = "___"
+        '
+        'Lblc3
+        '
+        Me.Lblc3.AutoSize = True
+        Me.Lblc3.Location = New System.Drawing.Point(199, 186)
+        Me.Lblc3.Name = "Lblc3"
+        Me.Lblc3.Size = New System.Drawing.Size(25, 13)
+        Me.Lblc3.TabIndex = 5
+        Me.Lblc3.Text = "___"
+        '
+        'Lblc1
+        '
+        Me.Lblc1.AutoSize = True
+        Me.Lblc1.Location = New System.Drawing.Point(137, 186)
+        Me.Lblc1.Name = "Lblc1"
+        Me.Lblc1.Size = New System.Drawing.Size(25, 13)
+        Me.Lblc1.TabIndex = 4
+        Me.Lblc1.Text = "___"
+        '
+        'LblCp
+        '
+        Me.LblCp.AutoSize = True
+        Me.LblCp.Location = New System.Drawing.Point(18, 212)
+        Me.LblCp.Name = "LblCp"
+        Me.LblCp.Size = New System.Drawing.Size(93, 13)
+        Me.LblCp.TabIndex = 3
+        Me.LblCp.Text = "Computer's Score:"
+        '
+        'LblCDealt
+        '
+        Me.LblCDealt.AutoSize = True
+        Me.LblCDealt.Location = New System.Drawing.Point(18, 186)
+        Me.LblCDealt.Name = "LblCDealt"
+        Me.LblCDealt.Size = New System.Drawing.Size(113, 13)
+        Me.LblCDealt.TabIndex = 2
+        Me.LblCDealt.Text = "This computer is dealt:"
+        '
+        'LblPlayer
+        '
+        Me.LblPlayer.AutoSize = True
+        Me.LblPlayer.Location = New System.Drawing.Point(18, 69)
+        Me.LblPlayer.Name = "LblPlayer"
+        Me.LblPlayer.Size = New System.Drawing.Size(77, 13)
+        Me.LblPlayer.TabIndex = 1
+        Me.LblPlayer.Text = "Player's Score:"
+        '
+        'LblPDealt
+        '
+        Me.LblPDealt.AutoSize = True
+        Me.LblPDealt.Location = New System.Drawing.Point(18, 29)
+        Me.LblPDealt.Name = "LblPDealt"
+        Me.LblPDealt.Size = New System.Drawing.Size(97, 13)
+        Me.LblPDealt.TabIndex = 0
+        Me.LblPDealt.Text = "This player is dealt:"
+        '
+        'Lblp3
+        '
+        Me.Lblp3.AutoSize = True
+        Me.Lblp3.Location = New System.Drawing.Point(179, 29)
+        Me.Lblp3.Name = "Lblp3"
+        Me.Lblp3.Size = New System.Drawing.Size(25, 13)
+        Me.Lblp3.TabIndex = 9
+        Me.Lblp3.Text = "___"
+        '
+        'btnDraw
+        '
+        Me.btnDraw.Location = New System.Drawing.Point(166, 77)
+        Me.btnDraw.Name = "btnDraw"
+        Me.btnDraw.Size = New System.Drawing.Size(96, 26)
+        Me.btnDraw.TabIndex = 14
+        Me.btnDraw.Text = "Draw Card"
+        Me.btnDraw.UseVisualStyleBackColor = True
+        '
+        'btnCheck
+        '
+        Me.btnCheck.Location = New System.Drawing.Point(25, 120)
+        Me.btnCheck.Name = "btnCheck"
+        Me.btnCheck.Size = New System.Drawing.Size(99, 25)
+        Me.btnCheck.TabIndex = 15
+        Me.btnCheck.Text = "Check Scores"
+        Me.btnCheck.UseVisualStyleBackColor = True
+        '
+        'btnPlayAgn
+        '
+        Me.btnPlayAgn.Location = New System.Drawing.Point(166, 212)
+        Me.btnPlayAgn.Name = "btnPlayAgn"
+        Me.btnPlayAgn.Size = New System.Drawing.Size(96, 26)
+        Me.btnPlayAgn.TabIndex = 16
+        Me.btnPlayAgn.Text = "Play Again"
+        Me.btnPlayAgn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.btnPlayAgn)
+        Me.Controls.Add(Me.btnCheck)
+        Me.Controls.Add(Me.btnDraw)
+        Me.Controls.Add(Me.Lblp5)
+        Me.Controls.Add(Me.Lblp1)
+        Me.Controls.Add(Me.Lblc2)
+        Me.Controls.Add(Me.Lblpscore)
+        Me.Controls.Add(Me.Lblp3)
+        Me.Controls.Add(Me.Lblp2)
+        Me.Controls.Add(Me.Lblcscore)
+        Me.Controls.Add(Me.Lblp4)
+        Me.Controls.Add(Me.Lblc3)
+        Me.Controls.Add(Me.Lblc1)
+        Me.Controls.Add(Me.LblCp)
+        Me.Controls.Add(Me.LblCDealt)
+        Me.Controls.Add(Me.LblPlayer)
+        Me.Controls.Add(Me.LblPDealt)
+        Me.Name = "Form1"
+        Me.Text = "Game of 21"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Lblp5 As System.Windows.Forms.Label
+    Friend WithEvents Lblp1 As System.Windows.Forms.Label
+    Friend WithEvents Lblc2 As System.Windows.Forms.Label
+    Friend WithEvents Lblpscore As System.Windows.Forms.Label
+    Friend WithEvents Lblp2 As System.Windows.Forms.Label
+    Friend WithEvents Lblcscore As System.Windows.Forms.Label
+    Friend WithEvents Lblp4 As System.Windows.Forms.Label
+    Friend WithEvents Lblc3 As System.Windows.Forms.Label
+    Friend WithEvents Lblc1 As System.Windows.Forms.Label
+    Friend WithEvents LblCp As System.Windows.Forms.Label
+    Friend WithEvents LblCDealt As System.Windows.Forms.Label
+    Friend WithEvents LblPlayer As System.Windows.Forms.Label
+    Friend WithEvents LblPDealt As System.Windows.Forms.Label
+    Friend WithEvents Lblp3 As System.Windows.Forms.Label
+    Friend WithEvents btnDraw As System.Windows.Forms.Button
+    Friend WithEvents btnCheck As System.Windows.Forms.Button
+    Friend WithEvents btnPlayAgn As System.Windows.Forms.Button
+
+End Class
---
+++ Game of 21/Game of 21/Form1.vb
@@ -0,0 +1,96 @@
+﻿Public Class Form1
+    Dim intp1 As Integer
+    Dim intp2 As Integer
+    Dim intp3 As Integer
+    Dim intp4 As Integer
+    Dim intp5 As Integer
+    Dim intc1 As Integer
+    Dim intc2 As Integer
+    Dim intc3 As Integer
+    Dim intdraw As Integer
+    Dim intpscore As Integer
+    Dim intcscore As Integer
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+        intp1 = Int(Rnd() * 10 + 1)
+        intp2 = Int(Rnd() * 10 + 1)
+        intc1 = Int(Rnd() * 10 + 1)
+        intc2 = Int(Rnd() * 10 + 1)
+        intc3 = Int(Rnd() * 10 + 1)
+
+        Lblp1.Text = intp1
+        Lblp2.Text = intp2
+        Lblc1.Text = intc1
+        Lblc2.Text = intc2
+        Lblc3.Text = intc3
+
+        Lblc1.Visible = False
+        Lblc2.Visible = False
+        Lblc3.Visible = False
+
+        Lblpscore.Text = "___"
+        Lblcscore.Text = "___"
+        Lblp3.Text = "___"
+        Lblp4.Text = "___"
+        Lblp5.Text = "___"
+
+        intp3 = 0
+        intp4 = 0
+        intp5 = 0
+        intdraw = 0
+
+    End Sub
+
+    Private Sub btnDraw_Click(sender As Object, e As EventArgs) Handles btnDraw.Click
+
+        intdraw = intdraw + 1
+
+        If (intdraw = 1) Then
+            intp3 = Int(Rnd() * 10 + 1)
+            Lblp3.Text = intp3
+        ElseIf (intdraw = 2) Then
+            intp4 = Int(Rnd() * 10 + 1)
+            Lblp4.Text = intp4
+        ElseIf (intdraw = 3) Then
+            intp5 = Int(Rnd() * 10 + 1)
+            Lblp5.Text = intp5
+        Else : MessageBox.Show("No more draw cards")
+        End If
+
+
+    End Sub
+
+    Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+
+        intpscore = intp1 + intp2 + intp3 + intp4 + intp5
+        intcscore = intc1 + intc2 + intc3
+
+        Lblpscore.Text = intpscore
+        Lblcscore.Text = intcscore
+
+        Lblc1.Visible = True
+        Lblc2.Visible = True
+        Lblc3.Visible = True
+
+        If (intpscore > 21 And intcscore > 21) Then
+            MessageBox.Show("Bust")
+        ElseIf (intpscore = intcscore) Then
+            MessageBox.Show("Draw")
+        ElseIf (intpscore > 21 And intcscore < 21) Then
+            MessageBox.Show("Computer Wins")
+        ElseIf (intpscore < 21 And intcscore > 21) Then
+            MessageBox.Show("Player Wins")
+        ElseIf (intpscore <= 21 And intcscore <= 21 And intpscore > intcscore) Then
+            MessageBox.Show("Player Wins")
+        ElseIf (intpscore <= 21 And intcscore <= 21 And intcscore > intpscore) Then
+            MessageBox.Show("Computer Wins")
+        End If
+
+    End Sub
+
+    Private Sub btnPlayAgn_Click(sender As Object, e As EventArgs) Handles btnPlayAgn.Click
+        Call Form1_Load(sender, e)
+    End Sub
+End Class
